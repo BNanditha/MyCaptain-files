3f7985184007017b2b317be2ad58fe095e1b9811
BNanditha <84661169+BNanditha@users.noreply.github.com>
2021-05-23 14:51:34 +0530

    Add files via upload
    Data strucure tasks

---
+++ Data structures.py
@@ -0,0 +1,17 @@
+#Data structures
+
+#Task 1
+
+pi = 3.17
+
+r = 1.1
+
+print("Radius of the circle: 1.1 \nArea of the circle:", pi * r * r)
+
+
+#Task 2
+
+f1 = """Filename: abc.py
+The extension of the file is: python"""
+
+print(f1)

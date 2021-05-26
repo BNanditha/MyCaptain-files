482580eef643d03cac1a4d2d2c6fd2c8aaae70a6
BNanditha <84661169+BNanditha@users.noreply.github.com>
2021-05-26 23:23:30 +0530

    Add files via upload

---
+++ Positive numbers.py
@@ -0,0 +1,15 @@
+#Task 2
+#All positive numbers in a range
+
+List1 = [6,7,-2,12,-81,0]
+for x in List1:
+    if x >= 0:
+        print(x, end=",")
+
+
+List2 = [8,9,-2,12,-13,20]
+for x in List2:
+    if x >= 0:
+        print(x, end=",")
+
+         

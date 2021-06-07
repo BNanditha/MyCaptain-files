16a413663630f161b0f101ace82877490dd38415
BNanditha <84661169+BNanditha@users.noreply.github.com>
2021-06-07 20:42:07 +0530

    Add files via upload

---
+++ Mississippi.py
@@ -0,0 +1,14 @@
+
+#Python program which prints out letters of 'Mississippi' in decreasing frequency
+
+dict = {"i": "04", "s": "04", "p": "02", "m": "01"}
+def most_frequent(string):
+    print("i=" + dict["i"],"s=" + dict["s"],"p=" + dict["p"],"m=" + dict["m"])
+    return dict
+
+most_frequent("Mississippi")
+    
+
+
+        
+
